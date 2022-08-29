371d542de25664815cca56de3a2306e6e748353c
Admin <Admin@DESKTOP-SQR286K>
2022-08-29 19:03:14 +0530

    STI

---
+++ SingleInheritence/src/com/tns/entity/Manager.java
@@ -0,0 +1,21 @@
+package com.tns.entity;
+
+	import javax.persistence.*;
+	@Entity
+
+	@DiscriminatorValue(value="MGR")  
+	public class Manager extends Employee {
+
+		private static final long serialVersionUID = 1L;
+		
+		private String departmentName;
+
+		public String getDepartmentName() {
+			return departmentName;
+		}
+
+		public void setDepartmentName(String departmentName) {
+			this.departmentName = departmentName;
+		}
+		
+}
